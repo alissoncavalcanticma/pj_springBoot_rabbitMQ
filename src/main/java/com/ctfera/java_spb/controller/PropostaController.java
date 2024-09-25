7ae4e2788072363544bd9c046cb25d95a5d3a789
Alisson Cavalcanti <alissoncavalcanticma@gmail.com>
2024-09-24 23:01:29 -0300

    Implementando PropostaService

--- src/main/java/com/ctfera/java_spb/controller/PropostaController.java
+++ src/main/java/com/ctfera/java_spb/controller/PropostaController.java
@@ -2,6 +2,7 @@ package com.ctfera.java_spb.controller;
 
 import com.ctfera.java_spb.dto.PropostaRequestDTO;
 import com.ctfera.java_spb.dto.PropostaResponseDTO;
+import com.ctfera.java_spb.service.PropostaService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,8 +13,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/proposta")
 public class PropostaController {
 
+    private PropostaService propostaService;
+
     @PostMapping
-    public ResponseEntity<PropostaResponseDTO> criar(@RequestBody PropostaRequestDTO){
-        return null;
+    public ResponseEntity<PropostaResponseDTO> criar(@RequestBody PropostaRequestDTO requestDTO){
+
+        PropostaResponseDTO responseDTO = propostaService.criar(requestDTO);
+
+        return ResponseEntity.ok(responseDTO);
     }
 }
---
+++ src/main/java/com/ctfera/java_spb/service/PropostaService.java
@@ -0,0 +1,11 @@
+package com.ctfera.java_spb.service;
+
+import com.ctfera.java_spb.dto.PropostaRequestDTO;
+import com.ctfera.java_spb.dto.PropostaResponseDTO;
+
+public class PropostaService {
+
+    public PropostaResponseDTO criar (PropostaRequestDTO requestDTO){
+        return null;
+    }
+}

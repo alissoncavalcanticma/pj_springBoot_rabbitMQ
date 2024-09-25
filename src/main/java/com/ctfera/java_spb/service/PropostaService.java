949bf3c9b1bc664dc0a6e0c49c2e35ffe35a7a85
Alisson Cavalcanti <alissoncavalcanticma@gmail.com>
2024-09-24 23:39:46 -0300

    Ajustando instanciamento do Service com lombok

--- src/main/java/com/ctfera/java_spb/service/PropostaService.java
+++ src/main/java/com/ctfera/java_spb/service/PropostaService.java
@@ -2,7 +2,9 @@ package com.ctfera.java_spb.service;
 
 import com.ctfera.java_spb.dto.PropostaRequestDTO;
 import com.ctfera.java_spb.dto.PropostaResponseDTO;
+import org.springframework.stereotype.Service;
 
+@Service //Anotação de camada service para definir a responsabilidade de gerência do Spring
 public class PropostaService {
 
     public PropostaResponseDTO criar (PropostaRequestDTO requestDTO){

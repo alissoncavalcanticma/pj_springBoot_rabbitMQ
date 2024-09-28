25c4c7dbcb18bf63fa56fc9ffed8592fc58b25fa
Alisson Cavalcanti <alissoncavalcanticma@gmail.com>
2024-09-28 06:31:49 -0300

    Implementing Mapper of ResponseDTO

--- src/main/java/com/ctfera/java_spb/service/PropostaService.java
+++ src/main/java/com/ctfera/java_spb/service/PropostaService.java
@@ -3,6 +3,7 @@ package com.ctfera.java_spb.service;
 import com.ctfera.java_spb.dto.PropostaRequestDTO;
 import com.ctfera.java_spb.dto.PropostaResponseDTO;
 import com.ctfera.java_spb.entity.Proposta;
+import com.ctfera.java_spb.mapper.PropostaMapper;
 import com.ctfera.java_spb.repository.PropostaRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -14,7 +15,8 @@ public class PropostaService {
     private PropostaRepository propostaRepository;
 
     public PropostaResponseDTO criar (PropostaRequestDTO requestDTO){
-        propostaRepository.save(new Proposta());
-        return null;
+        Proposta proposta =  PropostaMapper.INSTANCE.convertDtoToProposta(requestDTO);
+        propostaRepository.save(proposta);
+        return PropostaMapper.INSTANCE.convertEntityToDto(proposta);
     }
 }

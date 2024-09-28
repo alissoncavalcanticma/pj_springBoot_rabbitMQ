e09debaad6f85a3089f5a0a77fd0e8b8c90fa78c
Alisson Cavalcanti <alissoncavalcanticma@gmail.com>
2024-09-28 05:59:39 -0300

    Finalling interface of mapping

--- src/main/java/com/ctfera/java_spb/mapper/PropostaMapper.java
+++ src/main/java/com/ctfera/java_spb/mapper/PropostaMapper.java
@@ -5,11 +5,16 @@ import com.ctfera.java_spb.dto.PropostaResponseDTO;
 import com.ctfera.java_spb.entity.Proposta;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
 
 
 // Após criação da estrutura básica, foi usado o complie do mavem para criar a implementação do PropostaMapper
 @Mapper
 public interface PropostaMapper {
+
+    //Método para chamar a interface de mapeamento
+    PropostaMapper INSTANCE = Mappers.getMapper(PropostaMapper.class);
+
 
     @Mapping(target = "usuario.nome", source = "nome") //Mapeando campo final <- origemDTO
     @Mapping(target = "usuario.sobrenome", source = "sobrenome") //Mapeando campo final <- origemDTO
@@ -22,6 +27,10 @@ public interface PropostaMapper {
     @Mapping(target = "observacao", ignore=true) //ignorar mapeamento não presente no DTO
     Proposta convertDtoToProposta(PropostaRequestDTO propostaRequestDTO);
 
-
+    @Mapping(target = "nome", source = "usuario.nome")
+    @Mapping(target = "sobrenome", source = "usuario.sobrenome")
+    @Mapping(target = "telefone", source = "usuario.telefone")
+    @Mapping(target = "cpf", source = "usuario.cpf")
+    @Mapping(target = "renda", source = "usuario.renda")
     PropostaResponseDTO convertEntityToDto(Proposta proposta);
 }

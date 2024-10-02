87d354ce5fb193495f81d27982cc1eda88c54c25
Alisson Cavalcanti <alissoncavalcanticma@gmail.com>
2024-10-01 21:04:23 -0300

    Implements Cors class configuration

---
+++ src/main/java/config/CorsConfiguration.java
@@ -0,0 +1,19 @@
+package config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+//Para ativar o uso do Cors deve implementar a interface WebMvcConfigurer
+@Configuration  // Definindo annotation de configuração
+public class CorsConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry){
+        registry.addMapping("/**") // Informando que tudo após a raiz deve ser permitido
+                .allowedOrigins("http://localhost/") //Informando o host/origem
+                .allowedMethods("*");
+
+    }
+
+}

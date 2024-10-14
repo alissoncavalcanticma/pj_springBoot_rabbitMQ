f43c4be9db9717eb5a4c3a0c4f9c2c683e033eea
Alisson Cavalcanti <alissoncavalcanticma@gmail.com>
2024-10-14 20:53:37 -0300

    Creating @Bean de RabbitTempalate e MessageConverter

--- src/main/java/com/ctfera/java_spb/config/RabbitMQConfiguration.java
+++ src/main/java/com/ctfera/java_spb/config/RabbitMQConfiguration.java
@@ -2,8 +2,11 @@ package com.ctfera.java_spb.config;
 
 //import com.rabbitmq.client.ConnectionFactory;
 import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.connection.Connection;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Bean;
@@ -95,6 +98,25 @@ public class RabbitMQConfiguration {
 
 
 
+
+
+        //Criado um Bean MessageConverter com o Jackson para converter Message do Rabbit em JSON e usar no @Bean de RabbitTemplate criado
+        @Bean
+        public Jackson2JsonMessageConverter jackson2JsonMessageConverter(){
+            return new Jackson2JsonMessageConverter();
+        }
+
+        //Criando Bean de RabbitTemplate
+        // Definindo ConectionFactory
+        // Definindo MessageConverter
+        @Bean
+        public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory){
+            RabbitTemplate rabbitTemplate = new RabbitTemplate();
+            rabbitTemplate.setConnectionFactory(connectionFactory);
+            rabbitTemplate.setMessageConverter(jackson2JsonMessageConverter());
+
+            return rabbitTemplate;
+        }
 
 
 

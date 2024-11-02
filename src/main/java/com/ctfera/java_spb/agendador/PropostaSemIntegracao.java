66063f0e3d0d3df34526fe9c7e657ed62c17e7a3
Alisson Cavalcanti <alissoncavalcanticma@gmail.com>
2024-11-02 05:24:15 -0300

    implementing using logger of error and creating a separate method for update proposta;

--- src/main/java/com/ctfera/java_spb/agendador/PropostaSemIntegracao.java
+++ src/main/java/com/ctfera/java_spb/agendador/PropostaSemIntegracao.java
@@ -2,8 +2,11 @@ package com.ctfera.java_spb.agendador;
 
 //Classe para buscar no banco de dados as propostas sem integração e enviar novamente para a fila
 
+import com.ctfera.java_spb.entity.Proposta;
 import com.ctfera.java_spb.repository.PropostaRepository;
 import com.ctfera.java_spb.service.NotificacaoRabbitService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -16,6 +19,9 @@ public class PropostaSemIntegracao {
 
     private String exchange;
 
+    //Utilizando lib  org.slf4j
+    public final Logger logger = LoggerFactory.getLogger(PropostaSemIntegracao.class);
+
     public PropostaSemIntegracao(PropostaRepository propostaRepository, NotificacaoRabbitService notificacaoRabbitService,@Value("${spring.rabbitmq.propostapendente.exchange}") String exchange) {
         this.propostaRepository = propostaRepository;
         this.notificacaoRabbitService = notificacaoRabbitService;
@@ -26,12 +32,17 @@ public class PropostaSemIntegracao {
         propostaRepository.findAllByIntegradaIsFalse().forEach(proposta -> {
            try{
                notificacaoRabbitService.notificar(proposta, exchange);
-               proposta.setIntegrada(true);
-               propostaRepository.save(proposta);
+
+               atualizarProposta(proposta);
            }catch(RuntimeException ex){
-               System.out.println(ex);
+                logger.error(ex.getMessage());
            }
         });
+    }
+
+    private void atualizarProposta(Proposta proposta){
+        proposta.setIntegrada(true);
+        propostaRepository.save(proposta);
     }
 
 }

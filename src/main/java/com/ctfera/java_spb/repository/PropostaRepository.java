8c84368ce95faa55599ba72c6b9c93029b1f7631
Alisson Cavalcanti <alissoncavalcanticma@gmail.com>
2024-11-02 04:05:26 -0300

    Creating sign of method for the find integrada = false

--- src/main/java/com/ctfera/java_spb/repository/PropostaRepository.java
+++ src/main/java/com/ctfera/java_spb/repository/PropostaRepository.java
@@ -4,7 +4,11 @@ import com.ctfera.java_spb.entity.Proposta;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PropostaRepository extends CrudRepository<Proposta, Long> {
+
+    List<Proposta> findAllByIntegradaIsFalse();
 
 }
